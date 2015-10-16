9e583b87f96ead46924b3679c409a74b654436a6
crafteverywhere <1196377271@qq.com>
2015-10-16 21:24:27 +0800

    Update lang_checker.py

--- lang_checker.py
+++ lang_checker.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
-# Author crafteverywhere
-# minecraft中文本地化文本更新辅助工具
+# @Author crafteverywhere
+# @Title minecraft中文本地化文本更新辅助工具
 ##############################################
 # 使用说明：
 # 本脚本使用python3.X编写，运行前请先配置好python3.X环境。
@@ -10,13 +10,13 @@
 # 将新版本的en_US.lang丢入new文件夹。
 # 运行该脚本后，可以在out_put路径下发现导出的en_US.lang文件，在其基础上可以很方便地更新文本。
 # 导出的文本可以直接丢入mod中测试。
-# 新增条目以#NEW 开头
-# 变更条目以#BEFORE ，#AFTER 和#CHANGE 开头，分别是旧的和新的英文以及需要更变的中文条目。
+# 新增条目以#N 开头
+# 变更条目以#B ，#A 和#C 开头，分别是旧的和新的英文以及需要更变的中文条目。
 
-file_cn = open("old/zh_CN.lang", 'r+', encoding = 'UTF-8')
-file_en = open("new/en_US.lang", 'r+', encoding = 'UTF-8')
-file_en_old =  open("old/en_US.lang", 'r+', encoding = 'UTF-8')
-file_out = open("out_put/en_US.lang", 'w', encoding = 'UTF-8')
+file_cn = open("old/zh_CN.lang", 'r+', encoding='UTF-8')
+file_en = open("new/en_US.lang", 'r+', encoding='UTF-8')
+file_en_old = open("old/en_US.lang", 'r+', encoding='UTF-8')
+file_out = open("out_put/en_US.lang", 'w', encoding='UTF-8')
 
 en_list = list()
 
@@ -31,67 +31,69 @@ line_number = int()
 
 line_number = 0
 for line in file_en.readlines():
-	line_number += 1
-	print('[' + str(line_number) + ']' + 'Loading en_US.lang: ' + line)
-	if line != None and line[0] != '#' and '=' in line:
-		line_list = line.split('=', 1)
-		line_key = line_list[0]
-		line_value = line_list[1]
-		en_list.append(line_key)
-		en_dict[line_key] = line_value
-	else:
-		en_list.append('#craft loves comments' + line)
+    line_number += 1
+    print('[' + str(line_number) + ']' + 'Loading en_US.lang: ' + line)
+    if line != None and line[0] != '#' and '=' in line:
+        line_list = line.split('=', 1)
+        line_key = line_list[0]
+        line_value = line_list[1]
+        en_list.append(line_key)
+        en_dict[line_key] = line_value
+    else:
+        en_list.append('#craft loves comments' + line)
 
 print('en_US.lang has been loaded!')
 
 line_number = 0
 for line in file_en_old.readlines():
-	line_number += 1
-	print('[' + str(line_number) + ']' + 'Loading the old version of en_US.lang: ' + line)
-	if line != None and line[0] != '#' and '=' in line:
-		line_list = line.split('=', 1)
-		line_key = line_list[0]
-		line_value = line_list[1]
-		en_old_dict[line_key] = line_value
+    line_number += 1
+    print('[' + str(line_number) + ']' +
+          'Loading the old version of en_US.lang: ' + line)
+    if line != None and line[0] != '#' and '=' in line:
+        line_list = line.split('=', 1)
+        line_key = line_list[0]
+        line_value = line_list[1]
+        en_old_dict[line_key] = line_value
 
 print('the old version of en_US.lang has been loaded!')
 
 line_number = 0
 for line in file_cn.readlines():
-	line_number += 1
-	print('[' + str(line_number) + ']' + 'Loading zh_CN.lang: ' + line)
-	if line != None and line[0] != '#' and '=' in line:
-		line_list = line.split('=', 1)
-		line_key = line_list[0]
-		line_value = line_list[1]
-		cn_dict[line_key] = line_value
-		try:
-			auto_dict[en_old_dict[line_key]] = line_value
-		except:
-			print('No auto_dict entry!')
+    line_number += 1
+    print('[' + str(line_number) + ']' + 'Loading zh_CN.lang: ' + line)
+    if line != None and line[0] != '#' and '=' in line:
+        line_list = line.split('=', 1)
+        line_key = line_list[0]
+        line_value = line_list[1]
+        cn_dict[line_key] = line_value
+        try:
+            auto_dict[en_old_dict[line_key]] = line_value
+        except:
+            print('No auto_dict entry!')
 
 print('zh_CN.lang has been loaded!')
 
 line_number = 0
 for line in en_list:
-	line_number += 1
-	if '#craft loves comments' in line:
-		file_out.writelines(line.replace('#craft loves comments', ''))
-	else:
-		try:
-			if en_old_dict[line] != en_dict[line]:
-				file_out.writelines('#BEFORE ' + line + '=' + en_old_dict[line])
-				file_out.writelines('#AFTER ' + line + '=' + en_dict[line])
-				file_out.writelines('#CHANGE ' + line + '=' + cn_dict[line])
-				change += 1
-			else:
-				file_out.writelines(line + '=' + cn_dict[line])
-		except:
-			try:
-				file_out.writelines(line + '=' + auto_dict[en_dict[line]])
-			except:
-				file_out.writelines('#NEW ' + line + '=' + en_dict[line])
-				change += 1
+    line_number += 1
+    if '#craft loves comments' in line:
+        file_out.writelines(line.replace('#craft loves comments', ''))
+    else:
+        try:
+            if en_old_dict[line] != en_dict[line]:
+                file_out.writelines(
+                    '#B ' + line + '=' + en_old_dict[line])
+                file_out.writelines('#A ' + line + '=' + en_dict[line])
+                file_out.writelines('#C ' + line + '=' + cn_dict[line])
+                change += 1
+            else:
+                file_out.writelines(line + '=' + cn_dict[line])
+        except:
+            try:
+                file_out.writelines(line + '=' + auto_dict[en_dict[line]])
+            except:
+                file_out.writelines('#N ' + line + '=' + en_dict[line])
+                change += 1
 
 print('Done!')
 print('Line Numbers: ' + str(line_number))
